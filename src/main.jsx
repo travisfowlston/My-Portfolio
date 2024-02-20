a0623294d6670e843dc9fe92890eb417d0f90d05
Travis Fowlston <travisfowlston@ymail.com>
2024-02-19 18:03:45 -0800

    finished navbar and got all routes working, finished error page

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,45 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import ReactDOM from "react-dom/client";
+// Bringing in the required imports from 'react-router-dom' to set up application routing behavior
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+// This is importing the Bootstrap CSS file to style the application
+import "bootstrap/dist/css/bootstrap.min.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+// Bringing in the pages the router will use to conditionally show the appropriate views
+import App from "./App";
+import ErrorPage from "./pages/ErrorPage";
+import HomePage from "./pages/HomePage";
+import ProjectsPage from "./pages/ProjectsPage";
+import ResumePage from "./pages/ResumePage";
+import ContactPage from "./pages/ContactPage";
+
+// Define the accessible routes, and which components respond to which URL
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        index: true,
+        element: <HomePage />,
+      },
+      {
+        path: "projects",
+        element: <ProjectsPage />,
+      },
+      {
+        path: "resume",
+        element: <ResumePage />,
+      },
+      {
+        path: "contact",
+        element: <ContactPage />,
+      },
+    ],
+  },
+]);
+
+// Render the RouterProvider component
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <RouterProvider router={router} />
+);
---
+++ src/pages/ErrorPage.jsx
@@ -0,0 +1,25 @@
+import { useRouteError } from "react-router-dom";
+
+export default function ErrorPage() {
+  const error = useRouteError();
+  console.error(error);
+
+  return (
+    <div className="container">
+      <div className="row justify-content-center mt-5 p-4">
+        <div className="col-md-6 text-center">
+          <img
+            src="https://placekitten.com/400/300"
+            alt="Page Not Found"
+            className="img-fluid"
+          />
+          <h1 className="mt-4">404 - Page Not Found</h1>
+          <p className="lead mt-4">
+            <i>{error.statusText || error.message}</i>
+          </p>
+          <a class="btn btn-primary" href="/" role="button">Go Home!</a>
+        </div>
+      </div>
+    </div>
+  );
+}
